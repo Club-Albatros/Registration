cb839855f2e35fdfdad13b2ca6454cd7cef3b39f
donker <peter@bring2mind.net>
2014-08-15 15:44:20 +0200

    Catch error in GetValue

--- Components/DictionaryExtensions.vb
+++ Components/DictionaryExtensions.vb
@@ -3,6 +3,7 @@
  <System.Runtime.CompilerServices.Extension()> _
  Public Function GetValue(Of T)(bag As IDictionary, key As Object, defaultValue As T) As T
   Try
+   If bag(key) Is Nothing Then Return defaultValue
    Return CType(bag(key), T)
   Catch ex As Exception
    Return defaultValue
